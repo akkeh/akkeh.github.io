22bfe28c6688963cd213e3766eed1d6701bf1137
akkeh <akkeaccu@msn.com>
2019-04-19 12:07:19 +0200

    added blog to menu

--- menu.js
+++ menu.js
@@ -1,3 +1,3 @@
 function loadMenu() {
-    document.getElementById("menu").innerHTML = '<nav> <ul> <li><a href="index.html"> home </a></li> <li><a href="proj.html">projects</a></li> <li><a href="links.html">links</a></li> <li><a href="docs/cv_amhouben.pdf">CV</a></li></ul></nav>'
+    document.getElementById("menu").innerHTML = '<nav> <ul> <li><a href="index.html"> home </a></li> <li><a href="proj.html">projects</a></li> <li><a href="blog.html">blog</a></li> <li><a href="links.html">links</a></li> <li><a href="docs/cv_amhouben.pdf">CV</a></li></ul></nav>'
 };
