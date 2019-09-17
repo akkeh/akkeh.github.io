d100965a96da42f0510104925299f3c237e7c622
akkeh <akkeaccu@msn.com>
2019-09-17 14:20:50 +0200

    updated

--- apps/finr/Plot.js
+++ apps/finr/Plot.js
@@ -1,24 +1,52 @@
+class Line {
+    constructor(M, w, h, dw, dh, ylim) {
+        this.M = M;
+        this.x = new Float32Array(M).fill(0.0);
+        this.h = h; this.w = w;
+        this.dh = dh; this.dw = dw;
+        this.ylim = ylim;
+
+        this.strokeStyle = '#0000FF';
+        this.lineWidth = 5;
+    };  // constructor
+
+    draw(ctx) {
+        var pnt = this.getPoint(0);
+        ctx.beginPath();
+            ctx.moveTo(pnt[0], pnt[1]);
+            for(var n=1; n<this.M; n++) {
+                pnt = this.getPoint(n);
+                ctx.lineTo(pnt[0], pnt[1]);
+            };
+            ctx.lineWidth = this.lineWidth;
+            ctx.strokeStyle = this.strokeStyle;
+        ctx.stroke();
+    };  // draw()
+
+    getPoint(n) {
+        return [this.dw*n, this.h - this.dh*(this.x[n] - this.ylim[0])];
+    };  // getPoint()
+ 
+};  // class Line
+
 class Plot {
     constructor(c, M, ylim) {
         this.c = c;     // canvas
         this.ctx = c.getContext("2d");
         this.h = c.height; this.w = c.width;
         this.M = M;
-        this.x = new Float32Array(M);
         this.ylim = ylim;
         this.dh = this.h / (ylim[1]-ylim[0]);
         this.dw = this.w / M;
 
-        for(var n=0; n<M; n++) 
-            this.x[n] = 0;
+        this.lines = [new Line(M, this.w, this.h, this.dw, this.dh, this.ylim)];
 
         this.params = {};
-        this.params['strokeStyle'] = '#0000FF';
     };  // constructor()
-
-    getPoint(n) {
-        return [this.dw*n, this.h - this.dh*(this.x[n] - this.ylim[0])];
-    };  // getPoint()
+    
+    addLine(M) {
+        this.lines.push(new Line(M, this.w, this.h, this.dw, this.dh, this.ylim));
+    };  // addLine()
 
     draw() {
         this.ctx.clearRect(0, 0, this.w, this.h);
@@ -29,17 +57,8 @@ class Plot {
             this.ctx.strokeStyle = '#000000';
             this.ctx.lineWidth = 1;
         this.ctx.stroke();
-        // draw line:
-        var pnt = this.getPoint(0);
-        this.ctx.beginPath();
-            this.ctx.moveTo(pnt[0], pnt[1]);
-            for(var n=1; n<this.M; n++) {
-                pnt = this.getPoint(n);
-                this.ctx.lineTo(pnt[0], pnt[1]);
-            };
-            this.ctx.lineWidth = 5;
-            this.ctx.strokeStyle = this.params.strokeStyle;
-        this.ctx.stroke();
-            
+        // draw lines:
+        for(var i=0; i<this.lines.length; i++)
+            this.lines[i].draw(this.ctx);
     };  // draw();
-}
+};  // class Plot
--- apps/finr/finr.js
+++ apps/finr/finr.js
@@ -1,32 +1,128 @@
 function init() {
-    var M = 1000;
-    var blocksize = 256;
-    document.pltInput = new Plot(document.getElementById("input"), M, [-5, 5]);
+    document.M = 5000;
+    document.blocksize = 128;
+    document.Dt = 0.1;
+    document.fps = 51.2;
+
+    // input:
+    document.pltInput = new Plot(document.getElementById("input"), document.M, [-5, 5]);
     document.pltInput.params['type'] = "dc";
     document.pltInput.params['Imu'] = 0.0;
     document.pltInput.params['Isigma'] = 1.0;
     changeInputType("dc");
+    document.pltInput.addLine(document.M);
+    document.pltInput.lines[1].strokeStyle = '#FF0000';
+    document.pltInput.params['rho'] = 0.5;
 
-    document.pltVm = new Plot(document.getElementById("Vm"), M, [-3, 1]);
-    document.M = M;
-    document.blocksize = blocksize;
+    // neurons:
+    document.pltVm = new Plot(document.getElementById("Vm"), document.M, [-3, 5]);
+    document.pltVm.params['tauVa'] = 11.;
+    document.pltVm.params['Vtha'] = 1;
+    document.pltVm.params['Vpeaka'] = 5;
+    document.pltVm.params['Vreseta'] = -3;
+    document.pltVm.Sa = new Float32Array(document.M);
+    document.pltVm.Qab = 0.0
+    document.pltVm.params['tauQab'] = 5.0;
+    document.pltVm.params['qab'] = 5.0;
+
+    document.pltVm.addLine(document.M);
+    document.pltVm.lines[1].strokeStyle = '#FF0000';
+    document.pltVm.params['tauVb'] = 11.;
+    document.pltVm.params['Vthb'] = 1;
+    document.pltVm.params['Vpeakb'] = 5;
+    document.pltVm.params['Vresetb'] = -3;
+    document.pltVm.Sb = new Float32Array(document.M);
+    document.pltVm.Qba = 0.0; 
+    document.pltVm.params['tauQba'] = 5.0;
+    document.pltVm.params['qba'] = 5.0;
+
+    document.pltVm.params['Dba'] = 1;//Math.floor(1/document.Dt);   // delay a->b
+    document.pltVm.params['Wba'] = 0;   // weight a->b
+    document.pltVm.params['Dab'] = 1 //Math.floor(1/document.Dt);   // delay b->a
+    document.pltVm.params['Wab'] = 0.5;   // weight b->a
+
+
+    // spectrogram:
+    var maxFreq = 500;
+    var maxBin = Math.floor(maxFreq/(1000.0/document.Dt)*document.M);
+    document.pltSpec = new Plot(document.getElementById("spec"), maxBin, [0, 1]);
+    document.pltSpec.addLine(maxBin);
+    document.pltSpec.lines[1].strokeStyle = '#FF0000';
+
     document.n = 0;
-    setInterval(update, 500);
+    setInterval(update, (1000.0*document.Dt) * (document.blocksize/document.fps));
 };  // init()
 
 function update() {
+    // update input:
     var input = generateInput(document.pltInput.params['type'], document.blocksize, document.pltInput.params);
+    var inputA = generateInput(document.pltInput.params['type'], document.blocksize, document.pltInput.params);
+    var inputB = generateInput(document.pltInput.params['type'], document.blocksize, document.pltInput.params);
     for(var m = 0; m<document.blocksize; m++) {
         // update input:
-        document.pltInput.x[mod(document.n + m, document.M)] = input[m];
+        document.pltInput.lines[0].x[mod(document.n + m, document.M)] = document.pltInput.params['rho']*input[m] + (1-document.pltInput.params['rho'])*inputA[m];
+        document.pltInput.lines[1].x[mod(document.n + m, document.M)] = document.pltInput.params['rho']*input[m] + (1-document.pltInput.params['rho'])*inputB[m];
     };
-    document.n = mod(document.n+document.blocksize, document.M);
 
+    // update neurons:
+    var Iexta, Va, dVa;
+    var Va_ = document.pltVm.lines[0].x[mod(document.n-1, document.M)];
+    var Iextb, Vb, dVb, Inn;
+    var Vb_ = document.pltVm.lines[1].x[mod(document.n-1, document.M)];
+    for(var m=0; m<document.blocksize; m++) {
+        Iexta = document.pltInput.lines[0].x[mod(document.n+m, document.M)];
+        Iexta = 0;
+        document.pltVm.Qab = -document.Dt*document.pltVm.Qab/document.pltVm.params['tauQab'] + document.pltVm.Sb[mod(document.n-document.pltVm.params['Dab'], document.M)] * document.pltVm.params['Wab'];
+        Inna = document.pltVm.params['qab']*document.pltVm.Qab;
+
+        Iextb = document.pltInput.lines[1].x[mod(document.n+m, document.M)];
+        document.pltVm.Qba = -document.Dt*document.pltVm.Qba/document.pltVm.params['tauQba'] + document.pltVm.Sa[mod(document.n-document.pltVm.params['Dba'], document.M)] * document.pltVm.params['Wba'];
+        Innb= document.pltVm.params['qba']*document.pltVm.Qba;
+        Va = Va_;
+        Vb = Vb_;
+        dVa = (Iexta+Inna-Va) / document.pltVm.params['tauVa'];
+        dVb = (Iextb+Innb-Vb) / document.pltVm.params['tauVb'];
+        Va = Va + document.Dt*dVa;
+        Vb = Vb + document.Dt*dVb;
+        Va_ = Va;
+        Vb_ = Vb;
+        document.pltVm.Sa[mod(document.n+m, document.M)] = 0;
+        if(Va_ > document.pltVm.params['Vtha']) {
+            Va_ = document.pltVm.params['Vreseta'];
+            Va = document.pltVm.params['Vpeaka'];
+            document.pltVm.Sa[mod(document.n+m, document.M)] = 1;
+        };
+        document.pltVm.Sb[mod(document.n+m, document.M)] = 0;
+        if(Vb_ > document.pltVm.params['Vthb']) {
+            Vb_ = document.pltVm.params['Vresetb'];
+            Vb = document.pltVm.params['Vpeakb'];
+            document.pltVm.Sb[mod(document.n+m, document.M)] = 1;
+        };
+        document.pltVm.lines[0].x[mod(document.n+m, document.M)] = Va;
+        document.pltVm.lines[1].x[mod(document.n+m, document.M)] = Vb;
+    };
+
+    // update spectrogram:
+    var outRe = document.pltVm.lines[0].x.slice();
+    var outIm = new Float32Array(document.M).fill(0);
+    transform(outRe, outIm);    // fft.js
+    for(var m=0; m<document.pltSpec.M; m++) 
+        document.pltSpec.lines[0].x[m] = 2/document.M*Math.sqrt(outRe[m]*outRe[m] + outIm[m]*outIm[m]);
+    outRe = document.pltVm.lines[1].x.slice();
+    outIm = new Float32Array(document.M).fill(0);
+    transform(outRe, outIm);    // fft.js
+    for(var m=0; m<document.pltSpec.M; m++) 
+        document.pltSpec.lines[1].x[m] = 2/document.M*Math.sqrt(outRe[m]*outRe[m] + outIm[m]*outIm[m]);
+
+
+    document.n = mod(document.n+document.blocksize, document.M);
     draw();
 };  // update()
 
 function draw() {
     document.pltInput.draw();
+    document.pltVm.draw();
+    document.pltSpec.draw();
 };
 function changeInputType(type) {
     document.pltInput.params['type'] = type;
@@ -105,7 +201,7 @@ function generateInput(type, M, params) {
         break;
     };  //  switch(type)
     return x;
-};  // generateInput
+};  // generateInput()
 
 function mod(n, M) {
     return (n>=0) ? n%M : (M-((-n)%M));
